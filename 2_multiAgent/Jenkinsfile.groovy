691f37a09d4ff1422bbbd62e54503306ce88249d
georgetom-e <georgetom2000@gmail.com>
2025-03-09 12:19:43 +0530

    Added JenkinsFile

---
+++ 2_multiAgent/Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    stages {
+        stage('Agent_1_Python_Image') {
+            agent {
+                docker { image 'python:3.14.0a5-bookworm' }
+            }
+            steps {
+                echo 'python --version'
+                echo 'python print("Hello World!")'
+            }
+        }
+
+        stage('Agent_2_Node_Image') {
+            agent {
+                docker { image 'node:16-alpine' }
+            }
+            steps {
+                sh 'node --version'
+            }
+        }
+
+        stage('Agent_3_MySQL_Image') {
+            agent {
+                docker { image 'mysql:latest' }
+            }
+            steps {
+                sh 'SELECT NOW();'
+            }
+        }
+    }
+}
